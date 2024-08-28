1103bffdc925a1e2c41efda185f043539903c770
Grendlel <gabrielmachadoss4977@gmail.com>
2024-08-28 14:14:20 -0300

    Crud Completo

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template, request, flash
 app = Flask(__name__)
 from database import db
 from flask_migrate import Migrate
-from models import Usuario
+from models import Pedidos
 app.config['SECRET_KEY'] ='13db70a7b93420a4df3c9688dafcd60bab89a54f00a8902f1b442e92562660de'
 
 # drive://usuario:senha@servidor/banco_de_dados
-conexao = "mysql+pymysql://alunos:cefetmg@127.0.0.1/FlaskGabriel"
+conexao = "mysql+pymysql://alunos:cefetmg@127.0.0.1/PedidosGab"
 app.config['SQLALCHEMY_DATABASE_URI'] = conexao
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -17,12 +17,12 @@ migrate = Migrate(app, db)
 def index():
     return render_template('index.html')
 
-@app.route('/aula')
-@app.route('/aula/<nome>')
-@app.route('/aula/<nome>/<curso>')
-@app.route('/aula/<nome>/<curso>/<int:ano>')
-def aula(nome = 'Gabriel', curso = 'Informática', ano = 1):
-    dados = {'nome':nome,'curso':curso, 'ano':ano}
+@app.route('/Deposito')
+@app.route('/Deposito/<data_pedido>')
+@app.route('/Deposito/<data_pedido>/<valor_total>')
+@app.route('/Deposito/<data_pedido>/<valor_total>/<int:status>')
+def Deposito(data_pedido = '2000/01/01', valor_total = 0, status = 'offline'):
+    dados = {'data pedido':data_pedido,'valor total':valor_total, 'status':status}
     return render_template('aula.html', dados_curso=dados)
 
 @app.route('/form')
@@ -35,10 +35,10 @@ def dados():
     dados = request.form
     return render_template('dados.html', dados = dados)
 
-@app.route('/usuario')
-def usuario():
-    u = Usuario.query.all()
-    return render_template('usuario_lista.html', dados = u)
+@app.route('/Pedidos')
+def Pedidos():
+    u = Pedidos.query.all()
+    return render_template('Pedidos.html', dados = u)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,16 +1,16 @@
 from database import db
 
-class Usuario(db.Model):
-    __tablename__= "Usuario"
-    id = db.Column(db.Integer, primary_key = True)
-    nome =db.Column(db.String(100))
-    email =db.Column(db.String(100))
-    idade =db.Column(db.Integer)
+class Pedidos(db.Model):
+    __tablename__= "Pedidos"
+    id_pedido = db.Column(db.Integer, primary_key = True)
+    data_pedido =db.Column(db.Date)
+    valor_total =db.Column(db.Float(10, 2))
+    status =db.Column(db.String(20))
 
-    def __init__(self, nome, email, idade):
-        self.nome = nome
-        self.email = email
-        self.idade = idade
+    def __init__(self, data_pedido, valor_total, status):
+        self.data_pedido = data_pedido
+        self.valor_total = valor_total
+        self.status = status
 
     def __repr__(self):
-        return "<Usuario {}>".format(self.nome)
+        return "<Data do Pedido {}>".format(self.data_pedido, self.valor_total, self.status)
